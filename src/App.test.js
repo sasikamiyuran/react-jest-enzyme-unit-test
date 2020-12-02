1b88af69b8c7e906571e20562f8bfe320d787f0a
sasikamm <sasikamm@fortude.co>
2020-12-02 12:16:36 +0530

    commit changes - this is doing because of need to run 'npm run eject'.

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,16 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test("renders learn react link", () => {
+test("should renders without error", () => {
   const wrapper = shallow(<App />);
-  console.log(wrapper.debug());
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
 });
+
+test("should renders button", () => {});
+
+test("should renders counter display", () => {});
+
+test("should counter start at 0", () => {});
+
+test("should clicking on button increments counter desplay", () => {});
