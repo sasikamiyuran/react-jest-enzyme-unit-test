ac0c0dd4162c40c6a87cffe50075e1b75b282289
sasikamm <sasikamm@fortude.co>
2020-12-02 10:32:31 +0530

    Initial commite

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders learn react link", () => {
+  const wrapper = shallow(<App />);
+  console.log(wrapper.debug());
 });
